aa0424ffd319e89aa7ed4b796a5c7657662f8c1a
kushallegit <169062369+kushallegit@users.noreply.github.com>
2024-07-29 10:55:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeText(){
+    const elements = document.getElementsByClassName('text');
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].innerText = 'Text has been modified';
+    }
+}
